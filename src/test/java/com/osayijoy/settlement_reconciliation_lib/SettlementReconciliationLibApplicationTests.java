eada8a28a2a45bdfa10a50f143db018738b2d8da
Joy Osayi <osayijoy16@gmail.com>
2024-01-15 18:57:02 +0100

    Reconciliation lib

---
+++ src/test/java/com/osayijoy/settlement_reconciliation_lib/SettlementReconciliationLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osayijoy.settlement_reconciliation_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementReconciliationLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
